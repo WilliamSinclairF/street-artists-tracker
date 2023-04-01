7a785a1a01823dda161e3ad0952a601817511931
William S <willzy@outlook.com>
2023-04-01 00:30:27 -0400

    tweaks to fix vercel build errors

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,25 +1,28 @@
-import { Nav } from "@sat/components/nav/Nav"
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import { Nav } from '@sat/components/nav/Nav';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 
-function map() {
+function Map() {
   const session = useSession();
   const router = useRouter();
-  const { data: sessionData } = useSession();
-  // Navigating to Home is not authenticated 
+  // Navigating to Home is not authenticated
   useEffect(() => {
-    if (session.status === "unauthenticated") {
-      router.push("/");
+    if (session.status === 'unauthenticated') {
+      router
+        .push('/')
+        .then(() => null)
+        .catch(() => null);
     }
   }, [session, router]);
 
   return (
-    session.status === "authenticated" && (
-    <>
-    <Nav position="relative" />
-    <div className="text-2xl pt-5 text-center">This is where the map page will go!</div>
-    </>
-  ))
+    session.status === 'authenticated' && (
+      <>
+        <Nav position="relative" />
+        <div className="pt-5 text-center text-2xl">This is where the map page will go!</div>
+      </>
+    )
+  );
 }
-export default map
+export default Map;
