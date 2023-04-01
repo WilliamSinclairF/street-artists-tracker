fba34dba4bb7f134b242e84fa15153345fe8288e
Tyrell C <tyrellrcurry@gmail.com>
2023-04-01 15:12:05 -0700

    Added upcoming events module

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -72,6 +72,10 @@ function Profile() {
     });
   }
 
+  const getEventsByUser = api.eventRouter.getEventsCreatedByCurrentUser.useQuery();
+
+  console.log(getEventsByUser.data);
+
   return (
     session.status === 'authenticated' && (
       <>
@@ -88,7 +92,7 @@ function Profile() {
                       style={{
                         backgroundImage: `url(${sessionData?.user?.image || ''})`,
                         backgroundSize: 'cover',
-                        outline: '3px solid rgb(125 211 252)'
+                        outline: '3px solid rgb(125 211 252)',
                       }}></div>
                     <div className="mt-2">
                       {localStorage.getItem('isArtist') === 'true' ? (
@@ -101,61 +105,79 @@ function Profile() {
                 )}
               </div>
             </div>
-            <div className="right pl-8 pt-4">
-              <div className='flex gap-2'>
-              <h1 className='text-2xl font-semibold'>Profile Information</h1>
-              <div className="edit">
-                {editing ? (
-                  <button className="bg-green-300 px-2 py-1" onClick={saveHandler}>
-                    Save
-                  </button>
-                ) : (
-                  <button className="bg-slate-300 px-2 py-1" onClick={editHandler}>
-                    Edit
-                  </button>
-                )}
+            <div className="right flex w-full flex-wrap content-baseline gap-12 pl-8 pr-[10vw] pt-4">
+              <div className="min-w-[325px] self-start rounded-md bg-slate-100 p-3">
+                <div className="flex justify-between">
+                  <h1 className="text-2xl font-semibold">Profile Information</h1>
+                  <div className="edit float-right">
+                    {editing ? (
+                      <button className="rounded-md bg-green-600 px-2 py-1 text-white" onClick={saveHandler}>
+                        Save
+                      </button>
+                    ) : (
+                      <button className="rounded-md bg-blue-500 px-2 py-1 text-white" onClick={editHandler}>
+                        Edit
+                      </button>
+                    )}
+                  </div>
+                </div>
+                <div className="pt-3">
+                  <p className="pb-2">
+                    First name:{' '}
+                    {editing ? (
+                      <input
+                        className="ml-2 border-2 border-indigo-600 px-1"
+                        placeholder={firstName}
+                        value={firstNameInput}
+                        onChange={(e) => setFirstNameInput(e.target.value)}
+                      />
+                    ) : (
+                      firstName
+                    )}
+                  </p>
+                  <p className="pb-2">
+                    Last name:{' '}
+                    {editing ? (
+                      <input
+                        className="ml-2 border-2 border-indigo-600 px-1"
+                        placeholder={lastName}
+                        value={lastNameInput}
+                        onChange={(e) => setLastNameInput(e.target.value)}
+                      />
+                    ) : (
+                      lastName
+                    )}
+                  </p>
+                  <p className="pb-2">
+                    Description:{' '}
+                    {editing ? (
+                      <input
+                        className="border-2 border-indigo-600 px-1"
+                        placeholder={description}
+                        value={descriptionInput}
+                        onChange={(e) => setDescriptionInput(e.target.value)}
+                      />
+                    ) : (
+                      description
+                    )}
+                  </p>
+                </div>
               </div>
-              </div>
-              <div className='pt-3'>
-                <p className="pb-2">
-                  First name:{' '}
-                  {editing ? (
-                    <input
-                      className="border-2 border-indigo-600 px-1"
-                      placeholder={firstName}
-                      value={firstNameInput}
-                      onChange={(e) => setFirstNameInput(e.target.value)}
-                    />
-                  ) : (
-                    firstName
-                  )}
-                </p>
-                <p className="pb-2">
-                  Last name:{' '}
-                  {editing ? (
-                    <input
-                      className="border-2 border-indigo-600 px-1"
-                      placeholder={lastName}
-                      value={lastNameInput}
-                      onChange={(e) => setLastNameInput(e.target.value)}
-                    />
-                  ) : (
-                    lastName
-                  )}
-                </p>
-                <p className="pb-2">
-                  Description:{' '}
-                  {editing ? (
-                    <input
-                      className="border-2 border-indigo-600 px-1"
-                      placeholder={description}
-                      value={descriptionInput}
-                      onChange={(e) => setDescriptionInput(e.target.value)}
-                    />
-                  ) : (
-                    description
-                  )}
-                </p>
+              <div className="w-[40vw] min-w-[325px] max-w-[600px] self-start rounded-md bg-slate-100 p-4 py-2">
+                <h2 className="text-2xl font-semibold">Your Upcoming Events</h2>
+                <div className="events">
+                  <ul className="grid grid-cols-2 gap-4">
+                    {getEventsByUser.data?.map((event) => (
+                      <li className="my-2 rounded-md bg-slate-400 p-2">
+                        <p className="text-xl font-semibold">{event.title}</p>
+                        <p className="font-semibold text-sm">{event.date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }).replace(/(\d+)(st|nd|rd|th)/, '$1<span>$2</span>')}</p>
+
+                        <p className="text-md">{event.description}</p>
+                        <p className="text-md">Attendees: {event.attendees.length}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
