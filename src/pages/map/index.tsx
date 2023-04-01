84cd1b40403c6df5527b89a68eeb52481441946f
Tyrell C <tyrellrcurry@gmail.com>
2023-03-31 19:31:14 -0700

    Map and Profile routes created

---
+++ src/pages/map/index.tsx
@@ -0,0 +1,11 @@
+import { Nav } from "@sat/components/nav/Nav"
+
+function map() {
+  return (
+    <>
+    <Nav position="relative" />
+    <div className="text-2xl pt-5 text-center">This is where the map page will go!</div>
+    </>
+  )
+}
+export default map
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import { Nav } from "@sat/components/nav/Nav"
+
+function profile() {
+  return (
+    <>
+    <Nav position="relative" />
+    <div className="text-2xl pt-5 text-center">This is where the profile page will go!</div>
+    </>
+  )
+}
+export default profile
